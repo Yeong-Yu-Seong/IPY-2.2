6a320bd251f7cdf07991fd0baeee1b6ef44f42b4
Ellysha Adriana Puteri Mohamad Yusof <S10267273@connect.np.edu.sg>
2026-01-22 20:08:03 +0800

    Serve button interaction
    prep UI and interaction button

---
+++ IP Y2.2/Assets/XR button scripts/ServeBentoButton.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ServeBentoButton : MonoBehaviour
+{
+    public void OnServeBentoPressed()
+    {
+        Debug.Log("Serve Bento pressed");
+
+        // Later: trigger payment phase
+    }
+}
